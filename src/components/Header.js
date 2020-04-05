5b696da5a08a4a2ab7a57021430fe08ff40df76f
Arturs <artursaudr@gmail.com>
2020-04-06 01:30:33 +0300

    Main elements extracted to separate components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+    <div class="header">
+        <h1>Header</h1>
+        <p>Some pretty banner here.</p>
+    </div>
+    );
+}
+
+export default Header;
---
+++ src/components/Sidebar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return(
+        <div class="sidebar">
+        <a href="#home">Home</a>
+        <a href="#news">Battle DD</a>
+        <a href="#contact">Pets DD</a>
+        <a href="#about">Shop DD</a>
+        <a href="#about">Adventure DD</a>
+        <a href="#about">Guild DD</a>
+        <a href="#about">Sanctuary L</a>
+        <a href="#about">Summon L</a>
+        <a href="#about">Reputation L</a>
+        <a href="#about">Hero DD</a>
+        <a href="#about">Secret Shop L</a>
+        <a href="#about">Arena L</a>
+      </div>
+    );
+}
+
+export default Sidebar;
