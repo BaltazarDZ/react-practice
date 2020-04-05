79ab921aa558fffa2cd0eca3354fcfbf3257a709
Arturs <artursaudr@gmail.com>
2020-04-06 01:21:50 +0300

    Initial layout and color scheme

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
+
 function App() {
   return (
-    <div className="App">
-      Nothing here yet.
+    <div className="app">
+      <div class="sidebar">
+        <a href="#home">Home</a>
+        <a href="#news">Battle DD</a>
+        <a href="#contact">Pets DD</a>
+        <a href="#about">Shop DD</a>
+        <a href="#about">Adventure DD</a>
+        <a href="#about">Guild DD</a>
+        <a href="#about">Sanctuary L</a>
+        <a href="#about">Summon L</a>
+        <a href="#about">Reputation L</a>
+        <a href="#about">Hero DD</a>
+        <a href="#about">Secret Shop L</a>
+        <a href="#about">Arena L</a>
+      </div>
+      <div className='content'>
+        <div class="header">
+          <h1>Header</h1>
+          <p>Some pretty banner here.</p>
+        </div>
+
+        <div class="row">
+          <div class="column">
+            <h2>Column</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sit amet pretium urna. Vivamus venenatis velit nec neque ultricies, eget elementum magna tristique. Quisque vehicula, risus eget aliquam placerat, purus leo tincidunt eros, eget luctus quam orci in velit. Praesent scelerisque tortor sed accumsan convallis.</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
